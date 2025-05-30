e876243a63c346173b43e14f4f874177bfe11992
HaLucina <25haruhiro@gmail.com>
2025-05-31 06:23:21 +0900

    add obsidian setthing files

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,11 @@
+-- 
+vim.scriptencoding = 'utf-8'
+vim.opt.encoding = 'utf-8'
+vim.opt.fileencoding = 'utf-8'
+vim.wo.number = true
+vim.opt.swapfile = false
+
+
 -- Use the system clipboard for yank, delete, change and put operationG
 vim.opt.clipboard = "unnamedplus"
 
