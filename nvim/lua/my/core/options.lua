084066b86797d9189d6899bc79a3a6c7c8312caf
HaLucina <25haruhiro@gmail.com>
2025-06-20 15:36:15 +0900

    create init.lua

---
+++ nvim/lua/my/core/options.lua
@@ -0,0 +1,67 @@
+vim.cmd("let g:netrw_liststyle = 3")
+
+-- 透明な背景を設定
+vim.cmd [[
+  highlight Normal ctermbg=none guibg=none
+  highlight NonText ctermbg=none guibg=none
+]]
+
+local opt = vim.opt -- for conciseness
+
+
+
+vim.scriptencoding = 'utf-8'
+vim.wo.number = true
+opt.encoding = 'utf-8'
+opt.fileencoding = 'utf-8'
+
+-- Error detected while processing CursorMoved Autocommands for "<buffer=1>"..function <SNR>22_preview_color[41]..<SNR>22_get_hi_str:
+-- line    3:
+-- E121: Undefined variable: s:termguicolors
+-- 
+-- nvimで~/.dircolors を編集してカーソルを下に移動していくと上記のエラーがいちいち出てきた。
+-- 解決策は以下をnvim.luaに追記すること。
+-- 参考 ＞ https://github.com/neovim/neovim/issues/32097
+-- vim.opt.termguicolors = true
+
+
+-- line numbers
+opt.relativenumber = true -- show relative line numbers
+opt.number = true -- shows absolute line number on cursor line (when relative number is on)
+
+-- tabs & indentation
+opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
+opt.shiftwidth = 2 -- 2 spaces for indent width
+opt.expandtab = true -- expand tab to spaces
+opt.autoindent = true -- copy indent from current line when starting new one
+
+-- line wrapping
+opt.wrap = false -- disable line wrapping
+
+-- search settings
+opt.ignorecase = true -- ignore case when searching
+opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
+
+-- cursor line
+opt.cursorline = true -- highlight the current cursor line
+
+-- appearance
+
+-- turn on termguicolors for nightfly colorscheme to work
+-- (have to use iterm2 or any other true color terminal)
+opt.termguicolors = true
+opt.background = "dark" -- colorschemes that can be light or dark will be made dark
+opt.signcolumn = "yes" -- show sign column so that text doesn't shift
+
+-- backspace
+opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
+
+-- clipboard
+opt.clipboard:append("unnamedplus") -- use system clipboard as default register
+
+-- split windows
+opt.splitright = true -- split vertical window to the right
+opt.splitbelow = true -- split horizontal window to the bottom
+
+-- turn off swapfile
+opt.swapfile = false
