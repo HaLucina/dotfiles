7f9ec563990782d15bc3a1f967407a8ba44211e8
HaLucina <25haruhiro@gmail.com>
2025-07-07 15:18:46 +0900

    update

--- nvim/lua/my/core/ime.lua
+++ nvim/lua/my/core/ime.lua
@@ -1,18 +1,18 @@
 local uname = vim.uv.os_uname()
-local group = vim.api.nvim_create_augroup("kyoh86-conf-ime", {})
+local group = vim.api.nvim_create_augroup("neovim-ime-toggle", {})
 if uname.sysname == "Linux" then
-  if os.getenv("WSL_DISTRO_NAME") ~= "" then
+  if os.getenv("WSL_DISTRO_NAME") ~= "" then -- WSLでの設定
+    -- 参考> https://zenn.dev/vim_jp/articles/a8d4b091cd89f5
+    -- 以下の設定がされていること前提
+    -- $ gh --repo kyoh86/zenhan release download -p '*.exe' -O ~/dotfiles/esl/bin/zenhan.exe 
+    -- $ chmod +x ~/.local/bin/zenhan.exe 
+    -- Insertモードから抜けるときにIMEを自動でオフにする
     vim.api.nvim_create_autocmd("InsertLeave", {
       group = group,
       command = "silent! !zenhan.exe 0",
     })
   else
     -- 割愛: WSL以外の設定
-    vim.api.nvim_create_autocmd("InsertLeave", { 
-      callback = function()
-        vim.fn.jobstart({ "fcitx-remote", "-c" })
-      end,
-    })
   end
 else
   -- 割愛: Linux以外の設定
