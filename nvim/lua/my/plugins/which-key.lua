7b0023c4e2e25eb8eb1611b3af00c5d2f6a78275
HaLucina <25haruhiro@gmail.com>
2025-07-18 15:15:10 +0900

    which-key

--- nvim/lua/my/plugins/which-key.lua
+++ nvim/lua/my/plugins/which-key.lua
@@ -1,15 +1,44 @@
----
 return {
   "folke/which-key.nvim",
-  event = "VeryLazy", -- または "LazyFile" など、適切なイベントを選択
+  event = "VeryLazy",
   init = function()
-    -- プラグインがロードされる前に実行される設定
     vim.o.timeout = true
     vim.o.timeoutlen = 500
   end,
   opts = {
-    auto_register = true, -- これも重要
-    defaults = {
-      ["s"] = { name = "+My S-Keymaps" },
+  --   delay = 0,
+  --   spec = {
+  --     -- { "s", group = "[S]earch" },
+  --     { "<leader>", group = "test" },
+  --     {
+  --       mode = "i", -- insert mode mappings
+  --       { "<C-x><C-l>", desc = "Whole lines" },
+  --       { "<C-x><C-n>", desc = "Keywords in current file" },
+  --       { "<C-x><C-k>", desc = "Keywords in dictionary" },
+  --       { "<C-x><C-t>", desc = "Keywords in thesaurus" },
+  --       { "<C-x><C-i>", desc = "Keywords in current+included files" },
+  --       { "<C-x><C-]>", desc = "Tags" },
+  --       { "<C-x><C-f>", desc = "File names" },
+  --       { "<C-x><C-d>", desc = "Definitions/macros" },
+  --       { "<C-x><C-v>", desc = "Vim command-line" },
+  --       { "<C-x><C-u>", desc = "User-defined completion" },
+  --       { "<C-x><C-o>", desc = "Omni completion" },
+  --       { "<C-x><C-s>", desc = "Spelling suggestions" },
+  --       { "<C-x><C-z>", desc = "Stop completion" },
+  --     },
+  --   },
+    triggers = {
+      { "<leader>", mode = "nixsotc" },
+      { "s", mode = "nixsotc" },
     },
-  }
+  },
+  keys = {
+    {
+      "<leader>?",
+      function()
+        require("which-key").show({ global = true })
+      end,
+      desc = "Buffer Local Keymaps (which-key)",
+    },
+  },
+}
