c0e203b7c69466cc5283be7fb6a2486b18d71fad
HaLucina <58333749+HaLucina@users.noreply.github.com>
2026-03-14 01:21:07 +0900

    Update yazi.lua

--- nvim/.config/nvim/lua/my/plugins/yazi.lua
+++ nvim/.config/nvim/lua/my/plugins/yazi.lua
@@ -2,20 +2,23 @@ return {
   "mikavilpas/yazi.nvim",
   event = "VeryLazy",
   keys = {
-    -- nvim-treeの代わりに <leader>e でYaziを起動する設定
     {
       "<leader>e",
+      function() require("yazi").yazi() end,
+      --[[
+          require("yazi").yazi() を使うことで、NeovimのCWD（作業ディレクトリ）ではなく、
+          現在開いているファイルが存在するディレクトリを起点に Yazi を起動させる。
+          以下の書き方だと常にNeovimを起動した場所（プロジェクトルート等）が起点になる
+          "<cmd>Yazi<cr>",
+      --]]
       mode = { "n", "v" },
-      "<cmd>Yazi<cr>",
       desc = "Open yazi at the current file",
     },
   },
   opts = {
-    -- フローティングウィンドウの設定など
     open_for_directories = true,
     keymaps = {
       show_help = '<f1>',
     },
   },
 }
-
