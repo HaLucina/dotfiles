65ddd81a37cfe8854860e6e4e3649556c51c8252
HaLucina <58333749+HaLucina@users.noreply.github.com>
2026-03-26 07:21:29 +0900

    Update init.lua

--- nvim/.config/nvim/lua/my/plugins/init.lua
+++ nvim/.config/nvim/lua/my/plugins/init.lua
@@ -1,6 +1,6 @@
 -- Documents
--- (~/dotfiles/readmes/toc_nvim.md)
--- (~/dotfiles/readmes/toc_ALLKeyMap.md)
+-- (~/dotfiles/readmes/toc_nvim.md #plugin-init-lua)
+
 return {
   "nvim-lua/plenary.nvim", -- lua functions that many plugins use
 --  "christoomey/vim-tmux-navigator", -- tmux & split window navigation
